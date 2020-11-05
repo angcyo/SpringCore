ab5debdc48b195f47c068293f1c236f6345f824a
angcyo <angcyo@126.com>
2020-11-05 22:29:01 +0800

    + logback xml

--- core/src/main/kotlin/com/angcyo/spring/core/log/L.kt
+++ core/src/main/kotlin/com/angcyo/spring/core/log/L.kt
@@ -13,17 +13,49 @@ import org.slf4j.LoggerFactory
 
 object L {
 
-    val _log: Logger = LoggerFactory.getLogger("!angcyo!")
+    val _logAngcyo: Logger = LoggerFactory.getLogger("angcyo")
+    val _logHttp: Logger = LoggerFactory.getLogger("http")
+    val _logTemp: Logger = LoggerFactory.getLogger("temp")
+
+    /*----------------angcyo.log----------------------*/
 
     fun i(vararg log: Any?) {
-        _log.info(log.joinToString(" "))
+        _logAngcyo.info(log.joinToString(" "))
     }
 
     fun w(vararg log: Any?) {
-        _log.warn(log.joinToString(" "))
+        _logAngcyo.warn(log.joinToString(" "))
     }
 
     fun e(vararg log: Any?) {
-        _log.error(log.joinToString(" "))
+        _logAngcyo.error(log.joinToString(" "))
+    }
+
+    /*----------------http.log----------------------*/
+
+    fun ih(vararg log: Any?) {
+        _logHttp.info(log.joinToString(" "))
+    }
+
+    fun wh(vararg log: Any?) {
+        _logHttp.warn(log.joinToString(" "))
+    }
+
+    fun eh(vararg log: Any?) {
+        _logHttp.error(log.joinToString(" "))
+    }
+
+    /*----------------temp.log----------------------*/
+
+    fun it(vararg log: Any?) {
+        _logTemp.info(log.joinToString(" "))
+    }
+
+    fun wt(vararg log: Any?) {
+        _logTemp.warn(log.joinToString(" "))
+    }
+
+    fun et(vararg log: Any?) {
+        _logTemp.error(log.joinToString(" "))
     }
 }
