b10c700d2dd76a4b0f38d6e49a77be84f98857ed
angcyo <angcyo@126.com>
2021-07-06 09:58:51 +0800

    * fix http request
    (cherry picked from commit a8f746fe4dfd8cc273cc82ce99966c07406b8459)

--- http/src/main/java/com.angcyo/http/DslRequest.kt
+++ http/src/main/java/com.angcyo/http/DslRequest.kt
@@ -19,6 +19,39 @@ import java.nio.charset.Charset
  */
 class DslRequest {
 
+    companion object {
+        fun body(mediaType: String, byteArray: ByteArray): RequestBody {
+            return byteArray.toRequestBody(mediaType.toMediaTypeOrNull())
+        }
+
+        fun jsonBody(json: String): RequestBody {
+            return json.toRequestBody("application/json; charset=utf-8".toMediaTypeOrNull())
+        }
+
+        fun textBody(text: String): RequestBody {
+            return text.toRequestBody("text/plain".toMediaTypeOrNull())
+        }
+
+        fun formBody(form: Map<String, String?>?, charset: Charset? = Charsets.UTF_8): FormBody {
+            return FormBody.Builder(charset).apply {
+                form?.forEach { entry ->
+                    entry.value?.let {
+                        add(entry.key, it)
+                    }
+                }
+            }.build()
+        }
+
+        fun multipartBody(config: MultipartBody.Builder.() -> Unit): MultipartBody {
+            return MultipartBody.Builder().apply {
+                //setType()
+                //addPart()
+                //addFormDataPart()
+                config()
+            }.build()
+        }
+    }
+
     /**请求地址*/
     var url: String? = null
 
@@ -40,8 +73,14 @@ class DslRequest {
     var _call: Call? = null
 
     /**执行*/
-    fun doIt(): Call {
-        return request()
+    fun doIt(): Call? {
+        return try {
+            request()
+        } catch (e: Exception) {
+            e.printStackTrace()
+            _callback(_call, null, e)
+            null
+        }
     }
 
     fun request(): Call {
@@ -70,8 +109,8 @@ class DslRequest {
                 //base header
                 header("Accept", "*/*")
                 //header("Accept-Encoding", "gzip, deflate, br") //声明gzip压缩,需要手动解压.
-                //header("Cache-Control", "no-cache")
-                //header("Connection", "keep-alive")
+                header("Cache-Control", "no-cache")
+                header("Connection", "keep-alive")
 
                 requestUrl?.let {
                     val httpUrl = it.toHttpUrl()
@@ -80,7 +119,10 @@ class DslRequest {
                 }
 
                 header?.forEach { entry ->
-                    header(entry.key, entry.value)
+                    if (entry.key.toLowerCase() != "Accept-Encoding".toLowerCase()) {
+                        //不支持压缩
+                        header(entry.key, entry.value)
+                    }
                 }
             }
             .build()
@@ -109,8 +151,13 @@ class DslRequest {
                 }
             })
         } else {
-            val response = call.execute()
-            _callback(call, response, null)
+            try {
+                val response = call.execute()
+                _callback(call, response, null)
+            } catch (e: Exception) {
+                e.printStackTrace()
+                _callback(call, null, e)
+            }
         }
         _call = call
         return call
@@ -119,24 +166,25 @@ class DslRequest {
     //回调
     fun _callback(call: Call?, response: Response?, e: Exception?) {
         if (call == null || !call.isCanceled) {
-            if (response?.isSuccessful == true) {
-                onEndAction?.invoke(response, null)
-            } else {
-                if (response == null) {
-                    onEndAction?.invoke(null, e)
+            try {
+                if (response?.isSuccessful == true) {
+                    onEndAction?.invoke(response, null)
                 } else {
-                    onEndAction?.invoke(
-                        null,
-                        HttpDataException(response.message(), response.code())
-                    )
+                    if (response == null) {
+                        onEndAction?.invoke(null, e)
+                    } else {
+                        onEndAction?.invoke(
+                            null,
+                            HttpDataException(response.message(), response.code())
+                        )
+                    }
                 }
+            } catch (e: Exception) {
+                e.printStackTrace()
+                onEndAction?.invoke(null, e)
             }
         }
     }
-}
-
-fun body(mediaType: String, byteArray: ByteArray): RequestBody {
-    return byteArray.toRequestBody(mediaType.toMediaTypeOrNull())
 }
 
 fun jsonBody(json: String): RequestBody {
--- http/src/main/java/com/angcyo/spring/http/controller/HttpController.kt
+++ http/src/main/java/com/angcyo/spring/http/controller/HttpController.kt
@@ -2,8 +2,11 @@ package com.angcyo.spring.http.controller
 
 import com.angcyo.http.base.readString
 import com.angcyo.http.dslRequest
+import com.angcyo.spring.base.servlet.param
+import com.angcyo.spring.base.util.L
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.net.URI
 import javax.servlet.http.HttpServletRequest
 
 /**
@@ -18,13 +21,35 @@ class HttpController {
 
     @RequestMapping("/get")
     fun get(request: HttpServletRequest): String? {
-        val url = request.getHeader("url")
+        val hUrl = "url"
+        val url = request.getHeader(hUrl)
+        val noHeader = request.param("noHeader")
+
         if (url.isNullOrEmpty()) {
             return "url地址不能为空!"
         } else if (url.startsWith("http://") || url.startsWith("https://")) {
             var result: String? = null
             dslRequest {
                 this.url = url
+
+                if (noHeader != "true") {
+                    val header = hashMapOf<String, String>()
+                    this.header = header
+
+                    for (hKey in request.headerNames) {
+                        if (hKey != hUrl) {
+                            if (hKey == "host") {
+                                header["host"] = URI(url).host
+                            } else {
+                                header[hKey] = request.getHeader(hKey)
+                            }
+                        }
+                    }
+
+                    L.i("header↓")
+                    L.i(header)
+                }
+
                 async = false
                 onEndAction = { response, exception ->
                     response?.body()?.readString()?.let {
