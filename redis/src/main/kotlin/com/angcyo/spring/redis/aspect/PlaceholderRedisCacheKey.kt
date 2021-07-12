c6d8010a5e751c67558271f883f3cef37a73a66e
angcyo <angcyo@126.com>
2021-07-12 19:50:52 +0800

    * fix base

--- redis/src/main/kotlin/com/angcyo/spring/redis/aspect/PlaceholderRedisCacheKey.kt
+++ redis/src/main/kotlin/com/angcyo/spring/redis/aspect/PlaceholderRedisCacheKey.kt
@@ -1,8 +1,12 @@
 package com.angcyo.spring.redis.aspect
 
+import org.springframework.stereotype.Component
+
 /**
  * Email:angcyo@126.com
  * @author angcyo
  * @date 2021/07/12
  */
+
+@Component
 class PlaceholderRedisCacheKey : IRedisCacheKey
