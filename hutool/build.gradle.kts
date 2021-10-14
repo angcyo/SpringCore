114491d549e34e5cbdff59c7c61db7039d2ec655
angcyo <angcyo@126.com>
2021-10-14 14:38:30 +0800

    + hutool lib

---
+++ hutool/build.gradle.kts
@@ -0,0 +1,6 @@
+dependencies {
+    api("cn.hutool:hutool-all:5.7.14")
+
+    // https://mvnrepository.com/artifact/org.apache.poi/poi-ooxml
+    api("org.apache.poi:poi-ooxml:5.0.0")
+}
---
+++ hutool/src/main/kotlin/com/angcyo/hutool/Hutool.kt
@@ -0,0 +1,7 @@
+package com.angcyo.hutool
+
+/**
+ * Email:angcyo@126.com
+ * @author angcyo
+ * @date 2021/10/14
+ */
