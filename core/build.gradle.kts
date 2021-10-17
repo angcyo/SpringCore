6a5672b49fe6181495b5e64b7ec3e4232d341926
angcyo <angcyo@126.com>
2021-10-17 13:26:52 +0800

    * fix base

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -4,6 +4,9 @@ dependencies {
     //api(project(":SpringCore:swagger"))
     api(project(":SpringCore:mybatisplus"))
 
+    // https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-starter-thymeleaf
+    //api("org.springframework.boot:spring-boot-starter-thymeleaf") //2.5.5
+
     testImplementation("org.springframework.boot:spring-boot-starter-test") {
         exclude("org.junit.vintage", "junit-vintage-engine")
     }
