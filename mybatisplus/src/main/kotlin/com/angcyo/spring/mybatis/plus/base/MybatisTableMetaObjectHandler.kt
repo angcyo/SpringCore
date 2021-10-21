1a00795e7867e7bedb763aa5e3508ec90f6123f2
angcyo <angcyo@126.com>
2021-10-21 20:20:02 +0800

    * fix meta handler

--- mybatisplus/src/main/kotlin/com/angcyo/spring/mybatis/plus/base/MybatisTableMetaObjectHandler.kt
+++ mybatisplus/src/main/kotlin/com/angcyo/spring/mybatis/plus/base/MybatisTableMetaObjectHandler.kt
@@ -22,8 +22,18 @@ class MybatisTableMetaObjectHandler : MetaObjectHandler {
         //strictInsertFill(metaObject, "updatedAt", LocalDateTime::class.java, LocalDateTime.now())
         //strictInsertFill(metaObject, "deleteFlag", Int::class.java, 0)
 
-        strictInsertFill(metaObject, BaseAuditTable::createdAt.name, LocalDateTime::class.java, LocalDateTime.now())
-        strictInsertFill(metaObject, BaseAuditTable::updatedAt.name, LocalDateTime::class.java, LocalDateTime.now())
+        strictInsertFillOnHas(
+            metaObject,
+            BaseAuditTable::createdAt.name,
+            LocalDateTime::class.java,
+            LocalDateTime.now()
+        )
+        strictInsertFillOnHas(
+            metaObject,
+            BaseAuditTable::updatedAt.name,
+            LocalDateTime::class.java,
+            LocalDateTime.now()
+        )
 
         //审计
         //strictInsertFill(metaObject, "createdBy", String::class.java, "xxx")
@@ -35,11 +45,32 @@ class MybatisTableMetaObjectHandler : MetaObjectHandler {
 
     override fun updateFill(metaObject: MetaObject?) {
         //setFieldValByName("updatedAt", LocalDateTime.now(), metaObject)
-        setFieldValByName(BaseAuditTable::updatedAt.name, LocalDateTime.now(), metaObject)
+        setFieldValByNameOnNoSet(BaseAuditTable::updatedAt.name, LocalDateTime.now(), metaObject)
 
         //审计
         //setFieldValByName("updatedBy", "xxx", metaObject)
 
         //setFieldValByName(BaseAuditTable::updatedBy.name, "$userId", metaObject)
+    }
+
+    /**如果有字段, 则填充值, 否则跳过*/
+    fun <T, E : T?> strictInsertFillOnHas(
+        metaObject: MetaObject?,
+        fieldName: String?,
+        fieldType: Class<T>?,
+        fieldVal: E
+    ): MetaObjectHandler? {
+        if (metaObject?.hasSetter(fieldName) == true) {
+            return strictInsertFill(metaObject, fieldName, fieldType, fieldVal)
+        }
+        return null
+    }
+
+    /**如果字段未设置值, 则设置, 否则使用默认的*/
+    fun setFieldValByNameOnNoSet(fieldName: String?, fieldVal: Any?, metaObject: MetaObject?): MetaObjectHandler? {
+        if (getFieldValByName(fieldName, metaObject) == null) {
+            return setFieldValByName(BaseAuditTable::updatedAt.name, LocalDateTime.now(), metaObject)
+        }
+        return null
     }
 }
