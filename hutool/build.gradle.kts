6bc4088c2bc038df9ac7b94f39e91e55111e7252
angcyo <angcyo@126.com>
2021-10-27 14:30:36 +0800

    * fix gradle depend

--- hutool/build.gradle.kts
+++ hutool/build.gradle.kts
@@ -1,6 +1,10 @@
 dependencies {
     api("cn.hutool:hutool-all:5.7.14")
 
+    //https://www.hutool.cn/docs/#/poi/%E6%A6%82%E8%BF%B0?id=%e4%bb%8b%e7%bb%8d
     // https://mvnrepository.com/artifact/org.apache.poi/poi-ooxml
     api("org.apache.poi:poi-ooxml:5.0.0")
+
+    //https://mvnrepository.com/artifact/xerces/xercesImpl
+    api("xerces:xercesImpl:2.12.1")
 }
