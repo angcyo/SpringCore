5caa426776e62696b028aaade64c62282d52db51
angcyo <angcyo@126.com>
2021-10-27 18:06:03 +0800

    * fix gradle

--- hutool/build.gradle.kts
+++ hutool/build.gradle.kts
@@ -7,4 +7,9 @@ dependencies {
 
     //https://mvnrepository.com/artifact/xerces/xercesImpl
     api("xerces:xercesImpl:2.12.1")
+
+    //https://www.hutool.cn/docs/#/system/Oshi%E5%B0%81%E8%A3%85-OshiUtil
+    //基于JNA的操作系统和硬件信息库 https://github.com/oshi/oshi
+    //https://mvnrepository.com/artifact/com.github.oshi/oshi-core
+    api("com.github.oshi:oshi-core:5.8.3")
 }
