b981a7cbe4edebda707b800a92636557ae373add
angcyo <angcyo@126.com>
2021-11-01 16:31:23 +0800

    * fix base

--- base/src/main/kotlin/com/angcyo/spring/base/extension/ApiException.kt
+++ base/src/main/kotlin/com/angcyo/spring/base/extension/ApiException.kt
@@ -7,6 +7,7 @@ package com.angcyo.spring.base.extension
  */
 
 /**[com.angcyo.spring.base.advice.BaseRestExceptionHandlerAdvice]*/
+//@ResponseStatus(value = HttpStatus.NOT_IMPLEMENTED, code = HttpStatus.NOT_IMPLEMENTED, reason = "接口异常")
 open class ApiException(message: String = "接口异常", cause: Throwable? = null) : RuntimeException(message, cause)
 
 inline fun apiError(message: Any, cause: Throwable? = null): Nothing = throw ApiException(message.toString(), cause)
