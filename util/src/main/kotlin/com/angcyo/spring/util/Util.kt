e238d3e5627c8be207dbee81e9f05f57e10f8733
angcyo <angcyo@126.com>
2021-12-08 12:08:45 +0800

    + util

---
+++ util/src/main/kotlin/com/angcyo/spring/util/Util.kt
@@ -0,0 +1,30 @@
+package com.angcyo.spring.util
+
+import java.util.*
+
+/**
+ * Email:angcyo@126.com
+ * @author angcyo
+ * @date 2021/12/08
+ */
+
+var chars: Array<String> = arrayOf(
+    "a", "b", "c", "d", "e", "f",
+    "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s",
+    "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5",
+    "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I",
+    "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V",
+    "W", "X", "Y", "Z"
+)
+
+/**生成短的uuid*/
+fun generateShortUuid(length: Int = 8): String {
+    val shortBuffer = StringBuffer()
+    val uuid: String = UUID.randomUUID().toString().replace("-", "")
+    for (i in 0 until length) {
+        val str = uuid.substring(i * 4, i * 4 + 4)
+        val x = str.toInt(16)
+        shortBuffer.append(chars[x % 0x3E])
+    }
+    return shortBuffer.toString()
+}
