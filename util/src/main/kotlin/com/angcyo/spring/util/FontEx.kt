3a5ec1a7801cea31fa08c3363cd75476b4295b70
angcyo <angcyo@126.com>
2021-12-27 15:15:43 +0800

    * fix font

--- util/src/main/kotlin/com/angcyo/spring/util/FontEx.kt
+++ util/src/main/kotlin/com/angcyo/spring/util/FontEx.kt
@@ -1,7 +1,9 @@
 package com.angcyo.spring.util
 
-import sun.font.FontDesignMetrics
 import java.awt.Font
+import java.awt.FontMetrics
+import java.awt.Graphics2D
+import java.awt.image.BufferedImage
 
 /**
  * Email:angcyo@126.com
@@ -12,19 +14,24 @@ import java.awt.Font
 /**默认的字体*/
 val DEFAULT_FONT = Font("微软雅黑", Font.PLAIN, 16)
 
+val DEFAULT_GRAPHICS: Graphics2D
+    get() = BufferedImage(0, 0, BufferedImage.TYPE_INT_ARGB).graphics as Graphics2D
+
 /**测量文本的宽度*/
 fun String.textWidth(font: Font = DEFAULT_FONT): Int {
-    val metrics: FontDesignMetrics = FontDesignMetrics.getMetrics(font)
-    var width = 0
-
-    for (i in 0 until length) {
-        width += metrics.charWidth(get(i))
-    }
-    return width
+//    val metrics: FontDesignMetrics = FontDesignMetrics.getMetrics(font)
+//    var width = 0
+//
+//    for (i in indices) {
+//        width += metrics.charWidth(get(i))
+//    }
+//    return width
+    val metrics: FontMetrics = DEFAULT_GRAPHICS.getFontMetrics(font)
+    return metrics.stringWidth(this)
 }
 
 /**测量文本的高度*/
-fun String.textHeight(font: Font = DEFAULT_FONT): Int {
-    val metrics: FontDesignMetrics = FontDesignMetrics.getMetrics(font)
+fun textHeight(font: Font = DEFAULT_FONT): Int {
+    val metrics: FontMetrics = DEFAULT_GRAPHICS.getFontMetrics(font)
     return metrics.height
 }
